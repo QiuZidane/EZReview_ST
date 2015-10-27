dff5f7a387fdf6a382089adcf6e3f729aa8b40c4
QiuZidane <qiusdsdc@126.com>
2015-10-27 23:08:31 +0800

    2015102701

---
+++ Stocks/Stocks/BNRPortfolio.h
@@ -0,0 +1,13 @@
+//
+//  BNRPortfolio.h
+//  Stocks
+//
+//  Created by QiuZidane on 15/10/27.
+//  Copyright © 2015年 QiuZidane. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRPortfolio : NSObject
+
+@end
---
+++ Stocks/Stocks/BNRPortfolio.m
@@ -0,0 +1,13 @@
+//
+//  BNRPortfolio.m
+//  Stocks
+//
+//  Created by QiuZidane on 15/10/27.
+//  Copyright © 2015年 QiuZidane. All rights reserved.
+//
+
+#import "BNRPortfolio.h"
+
+@implementation BNRPortfolio
+
+@end
